1d919e5711b77a827a7503ef64d070358b603d80
Emily Chan <emc15@sfu.ca>
2020-02-11 15:16:12 -0800

    working connection to database established

--- Thrive_Mind_App/views.py
+++ Thrive_Mind_App/views.py
@@ -58,26 +58,60 @@ def createDatabase():
     # Construct connection string
     config = {
         'host':'thriveminddb.mysql.database.azure.com',
-        'user':'pythonuser@thriveminddb',
-        'password':'thrivemindSqlAzure!',
-        'database':'thriveminddb',
+        'user':'thriveadmin@thriveminddb',
+        'password':'Cmpt4741!',
+        'database':'users',
         'ssl_ca':'/Users/emleechxn/Downloads/BaltimoreCyberTrustRoot.crt.pem'
     }
     
-    conn = mysql.connector.connect(**config)
+    # conn = mysql.connector.connect(**config)
+    # conn.close()
+    # return ''
 
-    conn.close()
+    # if request.method == "POST":
+    #     details = request.form
+    #     firstName = details['fname']
+    #     lastName = details['lname']
+    #     conn = mysql.connection.cursor()
+    #     conn.execute("INSERT INTO Users(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+    #     mysql.connection.commit()
+    #     conn.close()
+    #     return 'success'
+    # return render_template('database.html')
+    try:
+        conn = mysql.connector.connect(**config)
+        print("Connection established")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with the user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        cursor = conn.cursor()
 
+         # Drop previous table of same name if one exists
+        cursor.execute("DROP TABLE IF EXISTS inventory;")
+        print("Finished dropping table (if existed).")
 
-        # if request.method == "POST":
-        #     details = request.form
-        #     firstName = details['fname']
-        #     lastName = details['lname']
-        #     cur = mysql.connection.cursor()
-        #     cur.execute("INSERT INTO Users(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
-        #     mysql.connection.commit()
-        #     cur.close()
-        #     return 'success'
-        # return render_template('database.html')
+        # Create table
+        cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
+        print("Finished creating table.")
+
+        # Insert some data into table
+        cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("banana", 150))
+        print("Inserted",cursor.rowcount,"row(s) of data.")
+        cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("orange", 154))
+        print("Inserted",cursor.rowcount,"row(s) of data.")
+        cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("apple", 100))
+        print("Inserted",cursor.rowcount,"row(s) of data.")
+
+        # Cleanup
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Done.")
+        return ''
 
 
