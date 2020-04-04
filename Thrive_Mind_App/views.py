70af4399617609efaed9e8c99faace0698040962
Emily Chan <emc15@sfu.ca>
2020-04-04 12:59:58 -0700

    preliminary connection to azure mongo db complete, previous sql now deprecated

--- Thrive_Mind_App/views.py
+++ Thrive_Mind_App/views.py
@@ -11,6 +11,54 @@ import json
 from flask import request
 from flask import abort
 
+import os
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+
+
+db_name = "healthcareservice"
+host = os.getenv("thriveminddb.mongo.cosmos.azure.com")
+port = 10255
+username = os.getenv("thriveminddb")
+password = os.getenv("3ZVHbEQKjE7AwuaAFW5fIosnK7F5lr9ECQj9WgMSrzft3TQps8EsluPUCbgB1TFwWnTq2dSjLDy6eZcQilIwFg==")
+args = "ssl=true&retrywrites=false&ssl_cert_reqs=CERT_NONE"
+
+connection_uri = "mongodb://thriveminddb:3ZVHbEQKjE7AwuaAFW5fIosnK7F5lr9ECQj9WgMSrzft3TQps8EsluPUCbgB1TFwWnTq2dSjLDy6eZcQilIwFg==@thriveminddb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@thriveminddb@"
+client = MongoClient(connection_uri)
+
+
+try:
+    info = client.server_info() # Forces a call.
+except ServerSelectionTimeoutError:
+    print("server is down.")
+
+db = client[db_name]
+user_collection = db['hid']
+
+# Save to the DB
+# user_collection.insert_one({"email": "amer@foobar.com"})
+
+# Query the DB
+for user in user_collection.find():
+    print(user)
+
+#####################
+
+# class User(mongo.DynamicDocument):
+#     email = mongo.StringField()
+
+
+# # Save to the DB
+# new_user = User(email="pedro@foobar.com").save()
+
+# # Query the DB
+# for user in User.objects():
+#     print(user.email)
+
+
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
